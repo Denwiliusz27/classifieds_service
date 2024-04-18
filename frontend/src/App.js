3a1cc10de31736ff4bca58c2be1e2f8fa39aaf90
Denwiliusz27 <danwielgosz@gmail.com>
2024-04-18 13:49:26 +0200

    reinitiate frontend project with typeScript

--- frontend/src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-      <div>
-        <p>Start of service</p>
-      </div>
-  );
-}
-
-export default App;
---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <p>Starting service</p>
+    </div>
+  );
+}
+
+export default App;
