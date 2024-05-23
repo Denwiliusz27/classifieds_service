fcec83cf9343654f2397374fa5b788b1b768a717
Denwiliusz27 <danwielgosz@gmail.com>
2024-05-23 14:52:11 +0200

    finished client registration

--- backend/cmd/api/api.go
+++ backend/cmd/api/api.go
@@ -16,8 +16,9 @@ type Application struct {
 func (app *Application) AllCities(w http.ResponseWriter, r *http.Request) {
 	cities, err := app.DB.AllCities()
 	if err != nil {
-		_ = app.errorJSON(w, err)
 		fmt.Println("error getting Cities from db: ", err)
+		_ = app.errorJSON(w, err)
+		return
 	}
 
 	_ = app.writeJSON(w, http.StatusOK, cities)
@@ -36,27 +37,32 @@ func (app *Application) CreateClient(w http.ResponseWriter, r *http.Request) {
 
 	client, err := app.DB.GetUserByEmailAndRole(newClient.Email, "client")
 	if client != nil {
+		log.Println("error: ", err)
 		log.Println("User with email '", newClient.Email, "' already exists")
-		_ = app.errorJSON(w, err)
+		_ = app.errorJSON(w, fmt.Errorf("klient o adresie email '%s' już istnieje", newClient.Email), http.StatusFound)
+		return
 	}
 
 	userId, err := app.DB.CreateUser(newClient)
 	if err != nil {
 		log.Println(err)
-		_ = app.errorJSON(w, err, http.StatusBadRequest)
+		_ = app.errorJSON(w, err)
+		return
 	}
 
 	clientId, err := app.DB.CreateClient(userId)
 	if err != nil {
 		log.Println(err)
-		_ = app.errorJSON(w, err, http.StatusBadRequest)
+		_ = app.errorJSON(w, err)
+		return
 	}
 
 	for _, address := range newClient.Addresses {
 		_, err := app.DB.CreateClientAddress(address, clientId)
 		if err != nil {
 			log.Println(err)
-			_ = app.errorJSON(w, err, http.StatusBadRequest)
+			_ = app.errorJSON(w, err)
+			return
 		}
 	}
 
---
+++ frontend/src/models/ClientAddress.ts
@@ -0,0 +1,7 @@
+
+export interface ClientAddress {
+    city_id: number;
+    street: string;
+    building_nr: number;
+    flat_nr: number;
+}
